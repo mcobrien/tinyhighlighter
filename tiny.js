cf8f8aad885da98ed503d6c67b7bf8c2ae140644
Michael O'Brien <michael@mcobrien.org>
2009-09-29 12:22:49 +0100

    it works!

--- tiny.js
+++ tiny.js
@@ -5,30 +5,71 @@
     var tag = /<[^>]+\/?>/g;
     var patterns = {
         "csharp": [
-            ["multiline", /\/\*[\s\S]*\*\//],   // multi line comments
-            ["singleline", /\/\/.*?\n/],        // single line comments
-            ["string", /'(?:\\'|[^'])'/],       // char literals
-            ["string", /@"(?:""|[^"])*"/],      // literal strings
-            ["string", /"(?:\\"|[^"])*"/],      // strings
-            ["keyword", /\b(?:while|volatile|void|virtual|using|ushort|unsafe|unchecked|ulong|uint|typeof|try|true|throw|this|switch|struct|string|static|stackalloc|sizeof|short|sealed|sbyte|return|ref|readonly|public|protected|private|params|override|out|operator|object|null|new|namespace|long|lock|is|internal|interface|int|in|implicit|if|goto|foreach|for|float|fixed|finally|false|extern|explicit|event|enum|else|double|do|delegate|default|decimal|continue|const|class|checked|char|catch|case|byte|break|bool|base|as|abstract|partial)\b/]
+            [   // multi-line comments
+                /\/\*[\s\S]*\*\//,
+                "multiline"
+            ],   
+            [   // single line comments
+                /\/\/.*?\n/, 
+                "singleline"
+            ],        
+            [   // char literals
+                /'(?:\\'|[^'])'/,
+                "string"
+            ],       
+            [   // literal strings
+                /@"(?:""|[^"])*"/, 
+                "string"
+            ],      
+            [   // strings
+                /"(?:\\"|[^"])*"/, 
+                "string"
+            ],      
+            [   // keywords
+                /\b(?:while|volatile|void|virtual|using|ushort|unsafe|unchecked|ulong|uint|typeof|try|true|throw|this|switch|struct|string|static|stackalloc|sizeof|short|sealed|sbyte|return|ref|readonly|public|protected|private|params|override|out|operator|object|null|new|namespace|long|lock|is|internal|interface|int|in|implicit|if|goto|foreach|for|float|fixed|finally|false|extern|explicit|event|enum|else|double|do|delegate|default|decimal|continue|const|class|checked|char|catch|case|byte|break|bool|base|as|abstract|partial)\b/,
+                "keyword"
+            ]
         ],
-        "vb": [
-            ["singleline", /'.*?\n/g],         // single line comments
-            ["string", /'(\\'|[^'])'/g],       // char literals
-            ["string", /@"(""|[^"])*"/g],      // literal strings
-            ["string", /"(\\"|[^"])*"/g],      // strings
-            ["keyword", /\b(Private|Public|Function|Sub|Me|True|False|New|Partial|Class|CType|Shared|End|ByVal|Handles|If|Else|Then|For|As|In|Next|Each|Protected|Return|Imports|Overridable)\b/g]
-        ],
+        // "vb": [
+        //     ["singleline", /'.*?\n/g],         // single line comments
+        //     ["string", /'(\\'|[^'])'/g],       // char literals
+        //     ["string", /@"(""|[^"])*"/g],      // literal strings
+        //     ["string", /"(\\"|[^"])*"/g],      // strings
+        //     ["keyword", /\b(Private|Public|Function|Sub|Me|True|False|New|Partial|Class|CType|Shared|End|ByVal|Handles|If|Else|Then|For|As|In|Next|Each|Protected|Return|Imports|Overridable)\b/g]
+        // ],
         "xml": [
-            ["multiline", /<\!--[\s\S]*?-->/g], // comments
+            [   // comments
+                /<\!--[\s\S]*?-->/g, 
+                "multiline"
+            ], 
             [tag,
-                ["string", /"[^"]*"/g],        // attribute strings
-                ["string", /'[^']*'/g],        // attribute strings with '
-                ["keyword", /[<>]/g],          // angle brackets
-                ["keyword", /^\?|\?$/],        // leading or trailing question marks for processing instructions
-                ["keyword", /\/$/],            // slash before > for self-closing tags
-                ["keyword", /^\/?[\w:.]+/g],   // element names
-                ["keyword2", /[\w:]+(?==)/g]   // attribute names
+                [         
+                    [   // leading or trailing question marks for processing instructions
+                        /^\?|\?$/,
+                        "keyword"
+                    ],  
+                    [
+                        // attribute strings
+                        /"[^"]*"/g,
+                        "string"
+                    ],        
+                    [   // attribute strings with '
+                        /'[^']*'/g,
+                        "string"
+                    ],        
+                    [   // angle brackets
+                        /\/>|[<>]/g,
+                        "keyword"
+                    ], 
+                    [   // attribute names
+                        /[\w:.\-]+(?==)/g,
+                        "keyword2"
+                    ],            
+                    [   // element names
+                        /\/?[\w:.]+/g,
+                        "keyword"
+                    ]
+                ]
             ]
         ]
     };
@@ -85,29 +126,40 @@
         return input.toString().match(re)[1];
     }
         
-    function highlight(src, tokenDefinitions) {
-        var names = tokenDefinitions.map(function(defn) { return defn[0]; });
-        var patterns = tokenDefinitions.map(reString);
+    function tokenize(src, tokenDefinitions) {
+        var names = tokenDefinitions.map(function(defn) { return defn[1]; });
+        var patterns = tokenDefinitions.map(function(defn) { return reString(defn[0]); });
 
         var bigPattern = new RegExp("(" + patterns.join(")|(") + ")", "g");
         var m = null, lastIndex = 0, all = [];
         while (m = bigPattern.exec(src)) {
             for (var i = 1; i < m.length; i++) {
                 if (typeof(m[i]) != "undefined") {
-                    all.push(escapeHTML(src.substring(lastIndex, m.index)));
-                    all.push('<span class="' + names[i - 1] + '">');
-                    all.push(escapeHTML(m[i]));
-                    all.push('</span>');
+                    all.push(src.substring(lastIndex, m.index));
+                    
+                    if (typeof(names[i - 1]) == "string") {
+                        all.push('__LT__span class="' + names[i - 1] + '"__GT__');
+                        all.push(m[i]);
+                        all.push('__LT__/span__GT__');
+                    }
+                    else {
+                        // this has sub patterns
+                        all = all.concat(tokenize(m[i], names[i - 1]));
+                    }
+                    
                     lastIndex = bigPattern.lastIndex;
                     break;
                 }
             }
         }
     
-        // MAYBE: speed things up by using __LT__ and __GT__ when adding <span> to all,
-        // then escape everything at the same time, and de-escape those
-        all.push(escapeHTML(src.substring(lastIndex)));
-        return all.join("");
+        all.push(src.substring(lastIndex));
+        return all;
+    }
+        
+    function highlight(src, tokenDefinitions) {
+        var all = tokenize(src, tokenDefinitions);
+        return escapeHTML(all.join("")).replace(/__LT__/g, "<").replace(/__GT__/g, ">");
     }
 
     var codeblocks = document.getElementsByTagName("pre");
